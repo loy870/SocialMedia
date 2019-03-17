c9ccc6cd2011080160ee4d3385eb70ef12654677
loy870 <louisyang92@gmail.com>
2019-03-17 15:14:01 -0700

    tested front end registration component

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -6,7 +6,7 @@ class Navbar extends Component {
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
         <div className="container">
-          <Link className="navbar-brand" to="/">DevConnector</Link>
+          <Link className="navbar-brand" to="/">mySocialMedia</Link>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#mobile-nav">
             <span className="navbar-toggler-icon"></span>
           </button>
@@ -24,7 +24,7 @@ class Navbar extends Component {
                 <Link className="nav-link" to = "/register">Sign Up</Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" href="/login">Login</Link>
+                <Link className="nav-link" to ="/login">Login</Link>
               </li>
             </ul>
           </div>
