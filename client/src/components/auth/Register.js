b1bfa1a76d39c7ea73a2a907287d9e1a20d27bb1
loy870 <louisyang92@gmail.com>
2019-03-18 23:06:38 -0700

    adding redux test routes

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import classnames from 'classnames';
+import {connect} from 'react-redux';
+import {registerUser} from '../../actions/authActions';
  
 class Register extends Component {
 
@@ -33,9 +35,11 @@ class Register extends Component {
             password2 : this.state.password2
         };
 
-        axios.post('/api/users/register', newUser)
-        .then(res => console.log(res.data))
-        .catch(err => this.setState({errors: err.response.data}));
+        this.props.registerUser(newUser);
+
+        // axios.post('/api/users/register', newUser)
+        // .then(res => console.log(res.data))
+        // .catch(err => this.setState({errors: err.response.data}));
     }
 
    render() {
@@ -84,5 +88,9 @@ class Register extends Component {
      )
    }
  }
+
+ const mapStateToProps = (state) => ({
+   auth: state.auth
+ })
  
- export default Register; 
+ export default connect(null, {registerUser})(Register); 
